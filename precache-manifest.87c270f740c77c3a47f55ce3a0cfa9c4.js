d87bff3e0a81a019d44163b7f5d8ee2d8dd68c99
Natalija Jovanovska <natalija.j@live.com>
2020-05-08 21:48:36 +0200

    Updates

---
+++ precache-manifest.87c270f740c77c3a47f55ce3a0cfa9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31852b6f1811e265e1121b775cfaed22",
+    "url": "/shopping-app/index.html"
+  },
+  {
+    "revision": "6f1cc98831c9728c5066",
+    "url": "/shopping-app/static/css/main.3cc3f388.chunk.css"
+  },
+  {
+    "revision": "73d6acc1cd2785885542",
+    "url": "/shopping-app/static/js/2.84423b87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-app/static/js/2.84423b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1cc98831c9728c5066",
+    "url": "/shopping-app/static/js/main.7dc4aa40.chunk.js"
+  },
+  {
+    "revision": "0a81ca41d26423c12d8b",
+    "url": "/shopping-app/static/js/runtime-main.c0c38a71.js"
+  }
+]);
